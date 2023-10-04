c52a168d79e6e030c740b33a249faa7c23bb9d84
batoolaltameemi <120500372+batoolaltameemi@users.noreply.github.com>
2023-10-04 10:25:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let insideOfillustration = document.getElementById("Layer_1");
+
+insideOfillustration.addEventListener("click", ()=> {
+document.getElementById("textbox").innerText= "You have clicked on my illustration"
+} )
